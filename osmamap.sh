843716460c4de3f7cada4de1fb01843025978f7f
iw1gfv <iw1gfv@gmail.com>
2016-01-06 18:01:52 +0100

    Spostati gli alberui da ways a pois

--- osmamap.sh
+++ osmamap.sh
@@ -4,5 +4,5 @@ source "./configurazione"
 
 
 #trasforma la mappa .osm in .map aggiungendo la bounding box
-/home/michele/osmosis/bin/osmosis  --rx file=$uscitaosm/bdtre-osm.osm  --mw file=$uscitamap/bdtre-osm.map tag-conf-file=tag-mapping.xml bbox=45.313529,7.5997925,45.4851692,7.9266357
+/home/michele/osmosis/bin/osmosis  --rx file=$uscitaosm/bdtre-osm.osm  --mw file=$uscitamap/bdtre-osm.map tag-conf-file=tag-mapping.xml bbox=44,6,47,10
 
