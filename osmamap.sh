a53b3a40229601b280ecbb0e349351e38d078b40
iw1gfv <iw1gfv@gmail.com>
2016-01-06 15:00:05 +0100

    corretto un errore

--- osmamap.sh
+++ osmamap.sh
@@ -1,2 +1,2 @@
 #trasforma la mappa .osm in .map aggiungendo la bounding box
-/home/michele/osmosis/bin/osmosis  --rx file=/home/michele/Scrivania/BDTRE/uscita-osm/merge.osm   --wb file=/home/michele/Scrivania/BDTRE/uscita-map/merge.pbf
+/home/michele/osmosis/bin/osmosis  --rb file=/home/michele/Scrivania/BDTRE/uscita-map/merge.pbf  --mw file=/home/michele/Scrivania/BDTRE/uscita-map/merge.map tag-conf-file=/home/michele/Scrivania/BDTRE/tag-mapping.xml bbox=45.313529,7.5997925,45.4851692,7.9266357
