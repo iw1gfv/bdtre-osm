e50daf107f45454c4abc0dfa615d2d9ae824e77c
iw1gfv <iw1gfv@gmail.com>
2016-01-06 14:52:19 +0100

    Modificato il comando per generare la mappa

--- osmamap.sh
+++ osmamap.sh
@@ -1,3 +1,2 @@
-nice /home/michele/osmosis/bin/osmosis  --rb file=/home/michele/Scrivania/BDTRE/uscita-map/merge.pbf  --mw file=/home/michele/Scrivania/BDTRE/uscita-map/merge.map tag-conf-file=/home/michele/Scrivania/BDTRE/tag-mapping.xml bbox=45.313529,7.5997925,45.4851692,7.9266357
-
-#/home/michele/osmosis/bin/osmosis  --rx file=/home/michele/Scrivania/BDTRE/uscita-osm/merge.osm   --wb file=/home/michele/Scrivania/BDTRE/uscita-map/merge.pbf
+#trasforma la mappa .osm in .map aggiungendo la bounding box
+/home/michele/osmosis/bin/osmosis  --rx file=/home/michele/Scrivania/BDTRE/uscita-osm/merge.osm   --wb file=/home/michele/Scrivania/BDTRE/uscita-map/merge.pbf
