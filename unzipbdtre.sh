d857cd3ff12d4d518c6a7e3d2b4cc1e50adeba58
lucinonno <lucio@galosso.it>
2016-01-13 00:47:44 +0100

    aggiunta verifica

--- unzipbdtre.sh
+++ unzipbdtre.sh
@@ -2,6 +2,13 @@
 #carica il file di configuraione delle variabili
 source "./configurazione"
 
+#verifica che sia presente la cacartellla con i file scaricati del BDTRE
+if [[ ! -d $bdtrez ]]
+then
+  echo "Non esiste la cartella con i file scaricati del BDTRE"
+  exit 1
+fi
+
 #entra nella directory dei files non zippati ed estrae li i files della bdtre
 cd $sbdtre
 unzip -o ../$sbdtrez/\*.zip
