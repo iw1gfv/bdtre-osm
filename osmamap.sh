f3ba4b59fe6e67660e333d0b7579b3c44027ce90
iw1gfv <iw1gfv@gmail.com>
2016-02-01 11:33:55 +0100

    Alcuni files rinominati

--- osmamap.sh
+++ osmamap.sh
@@ -7,7 +7,7 @@ source "./configurazione"
 #$osmosis -v --rx file=$uscitaosm/bdtre-osm.osm  --mw file=$uscitamap/bdtre-osm.map tag-conf-file=tag-mapping.xml bbox=44,6,47,10 type=hd
 
 #$osmosis -v --read-pbf-fast file=$uscitaosm/bdtre.pbf --rx file=$piemonteosm/piemonte.osm --merge --mw file=$uscitamap/bdtre-osm.map tag-conf-file=tag-mapping.xml bbox=44,6,47,10 type=hd
-$osmosis -v --read-pbf-fast file=$uscitaosm/bdtre.pbf workers=4 --b bufferCapacity=10000 --rbf file=$piemonteosm/piemonte.pbf workers=4 --b bufferCapacity=10000 --mw file=$uscitamap/bdtre-osm.map tag-conf-file=tag-mapping.xml bbox=44,6,47,10 type=hd
+$osmosis -v --read-pbf-fast file=$uscitaosm/bdtre.pbf --rbf file=$piemonteosm/piemonte_srtm.pbf --rbf file=$piemonteosm/piemonte.pbf  --merge --merge --mw file=$uscitamap/bdtre-osm.map tag-conf-file=tag-mapping.xml bbox=44,6,47,10 type=hd
 
 
 date
