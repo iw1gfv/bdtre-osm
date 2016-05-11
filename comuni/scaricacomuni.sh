e6b9ea536bd08747007acfa467c39f5896d0b32a
iw1gfv <iw1gfv@gmail.com>
2016-05-11 14:08:18 +0200

    Aggiunto uno script che scarica tutti gli id indipendentemente se nella lista o no

---
+++ comuni/scaricacomuni.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd ../BDTRE
+
+
+for i in {001001..103078} 
+do
+echo $i
+wget http://www.datigeo-piem-download.it/static/regp01/BDTRE2016_VECTOR/BDTRE_DATABASE_GEOTOPOGRAFICO_2016-LIMI_COMUNI_10_GAIMSDWL-$i-EPSG32632-SHP.zip
+
+done
+
